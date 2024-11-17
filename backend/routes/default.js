401c4f55d1c077762e56a0c745a182cd713f6050
Nadav Mozeson <117360467+NadavMozeson@users.noreply.github.com>
2024-11-17 12:20:34 +0200

    Revert "Initial Deployment of Main"

--- backend/routes/default.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const defaultRouter = express.Router()
-
-defaultRouter.get('/ping', async (req, res) => {
-    res.send('Pong!')
-})
-
-module.exports = defaultRouter
