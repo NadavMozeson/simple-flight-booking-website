6aa2d0c33219466b9dc2c82a639ef5b6fa61cdc5
NadavMozeson <nadavson02@gmail.com>
2024-11-17 14:15:19 +0200

    Added search to frontend

--- frontend/src/apiRequests.js
+++ frontend/src/apiRequests.js
@@ -21,3 +21,20 @@ export async function bookFlight(flight, name, id) {
     });
     return await response.json()
 }
+
+export async function searchFlight(origin = null, destination = null) {
+    const url = `${API_BASE_URL}/flights/search`
+    let tempBody = {}
+    if (origin) {
+        tempBody.origin = origin
+    }
+    if (destination) {
+        tempBody.destination = destination
+    }
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(tempBody),
+    });
+    return await response.json()
+}
